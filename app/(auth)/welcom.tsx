8b443d7b4a5a3575fc1b037dff8421cf325fd266
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-20 12:46:19 +0200

    new sytem

---
+++ app/(auth)/welcom.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Welcomeboards() {
+  return (
+    <View>
+      <Text>Welcome boards here</Text>
+     </View>
+  );
+}
