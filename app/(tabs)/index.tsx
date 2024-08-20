424ed0097f3e6885d6878f3aba0fc1df0cc68e73
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-20 11:28:10 +0200

    new code is added now

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Platform, View, Text, StatusBar } from 'react-native';
+import { Platform, View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
@@ -6,9 +7,9 @@ export default function HomeScreen() {
     <SafeAreaView className="bg-white flex-1 justify-center items-center">
       <Text className="text-red-900">Open up App.js to start working on your app!</Text>
       <Text className="text-lg mt-4">
-        {Platform.OS === 'android' ? 'Welcome on Android' : 'Welcome on iPhone'}
+        {Platform.OS === 'android' ? 'Welcome on Android !!' : 'Welcome on iPhone'}
       </Text>
-      <StatusBar style={"auto"}/>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
