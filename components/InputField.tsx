015a29dcce146b83d1e042d2030cbf706feaa0c9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-20 16:49:32 +0200

    checking m data Structure

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { View, Text, KeyboardAvoidingView } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-
+import {
+    TextInput,
+    View,
+    Text,
+    Image,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Platform,
+} from "react-native";
 export default function InputField({ labelStyle, label, icon, secureTextEntry = false,
     containerStyle, inputStyle, iconStyle, className, ...props
 }: any) {
@@ -9,9 +16,10 @@ export default function InputField({ labelStyle, label, icon, secureTextEntry = 
     return (
         <KeyboardAvoidingView>
             <TouchableWithoutFeedback>
-                <View className='my-2 w-full'> 
-                    <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>{label}</Text>
+                <View className='my-2 w-full'>
+                    <Text className={`text-l g font-JakartaSemiBold mb-3 ${labelStyle}`}>{label}</Text>
                 </View>
+                <View className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}></View>
             </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
     );
