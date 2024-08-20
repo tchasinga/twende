c26a311145cc08264c37cf62147b5f7197d0f1ce
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-20 11:55:15 +0200

    Trying to remove Index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../assets/fonts/PlusJakartaSans-BoldItalic.ttf'),
   });
 
   useEffect(() => {
@@ -26,8 +26,10 @@ export default function RootLayout() {
 
   return (
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen  options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
   );
 }
+
+// name="(tabs)"
