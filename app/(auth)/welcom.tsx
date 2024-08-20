c83871f8e8957617d34a3177a2151c8bd63dc9f1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-20 14:06:44 +0200

    new code is added now

--- app/(auth)/welcom.tsx
+++ app/(auth)/welcom.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { router } from 'expo-router';
+import React, { useRef } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from 'react-native-swiper';
 
 export default function Welcomeboards() {
+
+  const swiperRef = useRef<Swiper>(null);
+   
   return (
-    <SafeAreaView>
-      <Text>Welcome boards here</Text>
-     </SafeAreaView>
+    <SafeAreaView className='h-full flex items-center justify-between bg-white'>
+      <TouchableOpacity className='w-full flex justify-end items-end p-5 font-JakartaBold' onPress={() => {router.replace("/(auth)/sing-up")}}> 
+        <Text className='font-JakartaBold'>Skip it</Text>
+      </TouchableOpacity>
+
+      {/* Adding a swipper methode */}
+      <Swiper ref={swiperRef} loop={false} dot={<View className='w-[32px] h-[4px] mx-1 bg-[#E2E8F0]'/>}>
+      </Swiper>
+    </SafeAreaView>
   );
 }
- 
