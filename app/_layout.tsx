ef46f3bdd5a9fc5f3e2c6fb8178f5719be74855f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-21 09:04:33 +0200

    Token side checking

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
-import { Slot } from 'expo-router'
-
-
-// Adding a layout clerks
-
 
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
@@ -18,8 +13,6 @@ if (!publishableKey) {
     'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
   )
 }
-
-
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
