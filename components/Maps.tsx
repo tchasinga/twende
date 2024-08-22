872b6e7e9c0b8a498ab5fcd07b01ab4181f3f36f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-22 15:59:51 +0200

    New upgrading code is added now

--- components/Maps.tsx
+++ components/Maps.tsx
@@ -1,11 +1,21 @@
-import { Text , View} from "react-native";
+import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Maps = () => {
-    return (
-        <View>
-          <Text>Maps</Text>
-        </View>
-    )
-}
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full"
+      style={{ width: '100%', height: '100%' }}
+      mapType="satellite" 
+      showsUserLocation={true} 
+      followsUserLocation={true} 
+      zoomEnabled={true} 
+      zoomControlEnabled={true} 
+    >
+      <Text>Maps</Text>
+    </MapView>
+  );
+};
 
-export default Maps; 
+export default Maps;
