128d859ad11b5ed0a03d4054faed8f6439972a34
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-23 11:42:56 +0200

    new coomit

--- app/(auth)/sing-in.tsx
+++ app/(auth)/sing-in.tsx
@@ -31,7 +31,7 @@ export default function Singup() {
 
       if (signInAttempt.status === 'complete') {
         await setActive({ session: signInAttempt.createdSessionId })
-        router.replace('/')
+        router.replace('/(root)/(tabs)/home')
       } else {
         console.error(JSON.stringify(signInAttempt, null, 2))
       }
